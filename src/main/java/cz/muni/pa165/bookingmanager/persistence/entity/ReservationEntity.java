bf4ee0a340af8211e44a23d85d9ce50252bba0e3
Mojmír Odehnal <mojmir.odehnal@centrum.cz>
2016-10-19 18:12:12 +0200

    Updated ReservationEntity

--- src/main/java/cz/muni/pa165/bookingmanager/persistence/entity/ReservationEntity.java
+++ src/main/java/cz/muni/pa165/bookingmanager/persistence/entity/ReservationEntity.java
@@ -2,7 +2,6 @@ package cz.muni.pa165.bookingmanager.persistence.entity;
 
 import javax.persistence.*;
 import java.sql.Date;
-import java.util.Objects;
 
 /**
  * Entity class for a hotel room reservation
@@ -14,18 +13,11 @@ public class ReservationEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
-    @Column(nullable=false)
-    @ManyToOne
-    @JoinColumn(name="hotel_id")
-    private HotelEntity hotel;
-    
-    @Column(nullable=false)
+
     @ManyToOne
     @JoinColumn(name="room_id")
     private RoomEntity room;
     
-    @Column(nullable=false)
     @ManyToOne
     @JoinColumn(name="customer_id")
     private CustomerEntity customer;
@@ -42,12 +34,8 @@ public class ReservationEntity {
         return id;
     }
 
-    public HotelEntity getHotel() {
-        return hotel;
-    }
-
-    public void setHotel(HotelEntity hotel) {
-        this.hotel = hotel;
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public RoomEntity getRoom() {
@@ -82,54 +70,35 @@ public class ReservationEntity {
         this.endDate = endDate;
     }
 
-    @Override // Auto-generated
-    public int hashCode() {
-        int hash = 5;
-        hash = 97 * hash + Objects.hashCode(this.hotel);
-        hash = 97 * hash + Objects.hashCode(this.room);
-        hash = 97 * hash + Objects.hashCode(this.customer);
-        hash = 97 * hash + Objects.hashCode(this.startDate);
-        hash = 97 * hash + Objects.hashCode(this.endDate);
-        return hash;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ReservationEntity)) return false;
+
+        ReservationEntity that = (ReservationEntity) o;
+
+        if (!getRoom().equals(that.getRoom())) return false;
+        if (!getCustomer().equals(that.getCustomer())) return false;
+        if (!getStartDate().equals(that.getStartDate())) return false;
+        return getEndDate().equals(that.getEndDate());
+
     }
 
-    @Override // Auto-generated
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (!(obj instanceof ReservationEntity)) {
-            return false;
-        }
-        final ReservationEntity other = (ReservationEntity) obj;
-        if (!Objects.equals(this.hotel, other.hotel)) {
-            return false;
-        }
-        if (!Objects.equals(this.room, other.room)) {
-            return false;
-        }
-        if (!Objects.equals(this.customer, other.customer)) {
-            return false;
-        }
-        if (!Objects.equals(this.startDate, other.startDate)) {
-            return false;
-        }
-        if (!Objects.equals(this.endDate, other.endDate)) {
-            return false;
-        }
-        return true;
+    @Override
+    public int hashCode() {
+        int result = getRoom().hashCode();
+        result = 31 * result + getCustomer().hashCode();
+        result = 31 * result + getStartDate().hashCode();
+        result = 31 * result + getEndDate().hashCode();
+        return result;
     }
 
     @Override
     public String toString() {
-        return "ReservationEntity{" + "id=" + id + ", hotel=" + hotel 
-                + ", room=" + room + ", customer=" + customer 
-                + ", startDate=" + startDate + ", endDate=" + endDate + '}';
+        return "ReservationEntity{" + "id=" + id + ", room=" + room 
+                + ", customer=" + customer + ", startDate=" + startDate 
+                + ", endDate=" + endDate + '}';
     }
-    
     
     
 }
