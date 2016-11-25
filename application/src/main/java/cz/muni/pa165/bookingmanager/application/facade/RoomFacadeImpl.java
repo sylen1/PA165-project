8907933b4b1a5bf7a19a924cb0be6967751bdfa0
xgasior <o.gasior@hotmail.com>
2016-11-25 12:23:29 +0100

    Room facade iteration

--- application/src/main/java/cz/muni/pa165/bookingmanager/application/facade/RoomFacadeImpl.java
+++ application/src/main/java/cz/muni/pa165/bookingmanager/application/facade/RoomFacadeImpl.java
@@ -1,60 +1,84 @@
 package cz.muni.pa165.bookingmanager.application.facade;
 
 
-import cz.muni.pa165.bookingmanager.iface.dto.RoomDto;
-import cz.muni.pa165.bookingmanager.iface.util.PageResult;
-import cz.muni.pa165.bookingmanager.iface.util.RoomFilter;
 import cz.muni.pa165.bookingmanager.application.service.iface.RoomService;
+import cz.muni.pa165.bookingmanager.iface.dto.RoomDto;
 import cz.muni.pa165.bookingmanager.iface.facade.RoomFacade;
 import cz.muni.pa165.bookingmanager.iface.util.PageInfo;
+import cz.muni.pa165.bookingmanager.iface.util.PageResult;
+import cz.muni.pa165.bookingmanager.iface.util.RoomFilter;
 import cz.muni.pa165.bookingmanager.persistence.entity.RoomEntity;
+import org.dozer.Mapper;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.inject.Inject;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class RoomFacadeImpl implements RoomFacade {
 
+    private RoomService roomService;
+    private Mapper mapper;
+
     @Inject
-    private RoomService service;
+    public RoomFacadeImpl(RoomService roomService, Mapper mapper) {
+        this.roomService = roomService;
+        this.mapper = mapper;
+    }
 
     @Transactional(readOnly = true)
     public PageResult<RoomDto> findAll(PageInfo pageInfo) {
-        return convertAll(service.findAll(pageInfo));
+        return convertAll(roomService.findAll(pageInfo));
     }
 
     @Transactional
     public RoomDto registerRoom(RoomDto roomDto) {
-        return convertOne(service.registerRoom(convertOneBack(roomDto)));
+
+        RoomEntity entity = mapper.map(roomDto, RoomEntity.class);
+        RoomEntity saved = roomService.registerRoom(entity);
+
+        return mapper.map(saved, RoomDto.class);
     }
 
     @Transactional
     public RoomDto updateRoom(RoomDto roomDto) {
-        return convertOne(service.updateRoom(convertOneBack(roomDto)));
+
+        RoomEntity entity = mapper.map(roomDto, RoomEntity.class);
+        RoomEntity updated = roomService.updateRoom(entity);
+
+        return mapper.map(updated, RoomDto.class);
     }
 
     @Transactional(readOnly = true)
     public Optional<RoomDto> findByName(String name) {
-        return service.findByName(name).map(this::convertOne);
+        return roomService.findByName(name)
+                .map(x -> mapper.map(x, RoomDto.class));
     }
 
     @Transactional(readOnly = true)
-    public Optional<RoomDto> findById(Long id) {
-       return service.findById(id).map(this::convertOne);
+    public Optional<RoomDto> findById(Long id)
+    {
+       return roomService.findById(id)
+               .map(x -> mapper.map(x, RoomDto.class));
     }
 
     public PageResult<RoomDto> filterRooms(RoomFilter filter, PageInfo pageInfo) {
-        return convertAll(service.filterRooms(filter, pageInfo));
+        return convertAll(roomService.filterRooms(filter, pageInfo));
     }
 
     private PageResult<RoomDto> convertAll(PageResult<RoomEntity> all) {
-        return null; //TODO
-    }
-    private RoomEntity convertOneBack(RoomDto roomDto) {
-        return null; // TODO
-    }
-    private RoomDto convertOne(RoomEntity roomEntity) {
-        return null; // TODO
+
+        List<RoomDto> dtos = all.getEntries()
+                .stream()
+                .map(x -> mapper.map(x, RoomDto.class))
+                .collect(Collectors.toList());
+
+        PageResult<RoomDto> result = new PageResult<>();
+        mapper.map(all, result);
+        result.setEntries(dtos);
+
+        return result;
     }
 
 }
--- application/src/test/java/cz/muni/pa165/bookingmanager/application/facade/RoomFacadeTest.java
+++ application/src/test/java/cz/muni/pa165/bookingmanager/application/facade/RoomFacadeTest.java
@@ -1,7 +1,36 @@
 package cz.muni.pa165.bookingmanager.application.facade;
 
+import cz.muni.pa165.bookingmanager.application.service.iface.RoomService;
+import cz.muni.pa165.bookingmanager.application.service.iface.UserService;
+import cz.muni.pa165.bookingmanager.iface.facade.RoomFacade;
+import cz.muni.pa165.bookingmanager.iface.facade.UserFacade;
+import org.dozer.Mapper;
+import org.junit.Before;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.context.ApplicationContext;
+
+import javax.inject.Inject;
+
 /**
  * @author Gasior
  */
 public class RoomFacadeTest {
+    @Inject
+    private ApplicationContext context;
+
+    @Inject
+    private Mapper mapper;
+
+    private RoomFacade roomFacade;
+
+    @Mock
+    private RoomService roomService;
+
+
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+        roomFacade = new RoomFacadeImpl(roomService, context.getBean(Mapper.class));
+    }
 }
