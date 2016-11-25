83653d7154c80b04558360072dd57e4b18f0bc06
sylen1 <zeke4545@hotmail.com>
2016-11-25 19:01:10 +0100

    hotel service tests

--- application/src/test/java/cz/muni/pa165/bookingmanager/application/service/HotelServiceTest.java
+++ application/src/test/java/cz/muni/pa165/bookingmanager/application/service/HotelServiceTest.java
@@ -19,7 +19,8 @@ import javax.inject.Inject;
 import java.util.*;
 
 import static org.junit.Assert.*;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @ContextConfiguration(locations = { "/application-context.xml" } )
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -52,6 +53,18 @@ public class HotelServiceTest {
         hotels.add(createHotel("city H3"));
         when(hotelDao.findByCity("Brno", new PageRequest(0, 10)))
                 .thenReturn(new PageImpl<>(hotels, new PageRequest(0, 10), 3));
+
+        when(hotelDao.save(createHotel("update H1")))
+                .thenReturn(createHotel("update H1"));
+
+        List<HotelEntity> allHotels = new LinkedList<>();
+        allHotels.add(createHotel("all H1"));
+        allHotels.add(createHotel("all H2"));
+        allHotels.add(createHotel("all H3"));
+        allHotels.add(createHotel("all H4"));
+        allHotels.add(createHotel("all H5"));
+        when(hotelDao.findAll(new PageRequest(0, 10)))
+                .thenReturn(new PageImpl<>(allHotels, new PageRequest(0, 10), 5));
 
         hotelService = new HotelServiceImpl(hotelDao, context.getBean(Mapper.class));
     }
@@ -93,9 +106,39 @@ public class HotelServiceTest {
         expected.setPageSize(10);
 
         assertEquals(expected, actual);
-
     }
 
+    @Test(expected = NullPointerException.class)
+    public void updateHotelTest(){
+        HotelEntity hotel1 = createHotel("update H1");
+        hotel1.setId(1L);
+        HotelEntity returned = hotelService.updateHotelInformation(hotel1);
+        assertEquals(hotel1, returned);
+
+        HotelEntity hotelNull = createHotel("update H null");
+        hotelNull.setId(null);
+        hotelService.updateHotelInformation(hotelNull);
+    }
+
+    @Test
+    public void findAllTest(){
+        List<HotelEntity> allHotels = new LinkedList<>();
+        allHotels.add(createHotel("all H1"));
+        allHotels.add(createHotel("all H2"));
+        allHotels.add(createHotel("all H3"));
+        allHotels.add(createHotel("all H4"));
+        allHotels.add(createHotel("all H5"));
+
+        PageResult<HotelEntity> expected = new PageResult<>();
+        expected.setEntries(allHotels);
+        expected.setPageSize(10);
+        expected.setPageNumber(0);
+        expected.setPageCount(1);
+        expected.setTotalEntries(5);
+
+        PageResult<HotelEntity> actual = hotelService.findAll(new PageInfo(0, 10));
+        assertEquals(expected, actual);
+    }
 
     private HotelEntity createHotel(String seed){
         HotelEntity hotel = hotelCache.get(seed);
